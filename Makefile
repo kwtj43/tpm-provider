3169e6efb9e97be32d55c773e065327713b4dc1a
Kent Thompson <kent.thompson@intel.com>
2019-10-25 08:04:10 -0700

    CICD, start of unit test for tpm-provider

--- Makefile
+++ Makefile
@@ -3,18 +3,11 @@ GITCOMMIT := $(shell git describe --always)
 GITCOMMITDATE := $(shell git log -1 --date=iso-strict --pretty=format:%cd)
 VERSION := $(or ${GITTAG}, v1.0.0)
 
-tpmprovider:
-#	env GOOS=linux go build -gcflags=all="-N -l" -ldflags "-X intel/isecl/go-trust-agent/version.Version=$(VERSION) -X intel/isecl/go-trust-agent/version.GitHash=$(GITCOMMIT) -X intel/isecl/go-trust-agent/version.CommitDate=$(GITCOMMITDATE)" -o out/tagent
-	go test -c -o out/tpmprovider.test -gcflags=all="-N -l"
-
 # KWT
 # Pass the '-w' flag to the linker to omit the debug information (for example, go build -ldflags=-w prog.go).
-# https://golang.org/doc/gdb
-
-
-
-#build_test: tpmprovider
-#	go test -c -o ../out/tpmprovider.test
+# see https://golang.org/doc/gdb
+tpmprovider:
+	go test -c -o out/tpmprovider.test -gcflags=all="-N -l" -tags=gitlab
 
 all: tpmprovider
 
--- tpmprovider_test.go
+++ tpmprovider_test.go
@@ -1,4 +1,4 @@
-// +build !integration
+// +build gitlab
 
 /*
  * Copyright (C) 2019 Intel Corporation
@@ -7,9 +7,7 @@
 package tpmprovider
 
 import (
-	//"encoding/base64"
 	"encoding/hex"
-	"io/ioutil"
 	"testing"
 	"github.com/stretchr/testify/assert"
 	log "github.com/sirupsen/logrus"
@@ -29,13 +27,13 @@ func TestTpmVersion(t *testing.T) {
 	assert.NotEqual(t, version, 0)
 }
 
-func TestTakeOwnership(t *testing.T) {
-	tpmProvider, _ := NewTpmProvider()
-	defer tpmProvider.Close()
+// func TestTakeOwnership(t *testing.T) {
+// 	tpmProvider, _ := NewTpmProvider()
+// 	defer tpmProvider.Close()
 
-	rc := tpmProvider.TakeOwnership(TpmSecretKey)
-	assert.Equal(t, rc, 0)
-}
+// 	rc := tpmProvider.TakeOwnership(TpmSecretKey)
+// 	assert.Equal(t, rc, 0)
+// }
 
 // To run this test (more of a c debugging tool)...
 // Where:
@@ -59,40 +57,40 @@ func TestTakeOwnership(t *testing.T) {
 // cd to /tpmprovider and run 'go test -c' (compiles to tpmprovider/tpmprovider.test)
 // Run gbd unit test against tpmprovider.test (see launch.json)
 //
-func TestActivateCredential(t *testing.T) {
-	assert := assert.New(t)
+// func TestActivateCredential(t *testing.T) {
+// 	assert := assert.New(t)
 
-	tpmProvider, err := NewTpmProvider()
-	assert.NoError(err)
-	defer tpmProvider.Close()
-
-
-	// secret := "AQBZ4n0tHyIbb5watAUuGg+L4mL/z9r7LzoX8ujGtVST7OcoGU5enm5wMsA90Ufcfj7UxDv6FpYLqonxtl8LFvCB+4QNAA1EG4eGXIdzAXGU3JbTlXyr2DlRSBObMe/lf3pxiTPQctjoSsLQWw7BtOPpAVbp+OS+lTD8Dut+sva1TYoBnW9KAkU5qkLsKn8uBtb7ozX1rVteHDh1CPGYnKC3nfg5rdOuLlq3xGafE8osEHD/cXEKddtoUwMY+6zroJ7XwsaYvpsa7ArRhARViHKZFwtw9hMmBXR28E93iZDqthaQvfMxjrXBmsFbGptq91EaNp+G0XVH4mP0sJmlQpbI"
-	// secretBytes, err := base64.StdEncoding.DecodeString(secret)
-	// assert.NoError(err)
-
-	//credential := "ADQAID2qrkbHKt9ZEBb4RdhMh6esz52AHxuqd6LDDtI3pxwxMyYyEGNq0usYQAnW2H4hmggl"
+// 	tpmProvider, err := NewTpmProvider()
+// 	assert.NoError(err)
+// 	defer tpmProvider.Close()
 
 
-	credentialBytes, err := ioutil.ReadFile("/tmp/aikName")
-	secretBytes, err := ioutil.ReadFile("/tmp/secret.data") //[]byte("12345678")
+// 	// secret := "AQBZ4n0tHyIbb5watAUuGg+L4mL/z9r7LzoX8ujGtVST7OcoGU5enm5wMsA90Ufcfj7UxDv6FpYLqonxtl8LFvCB+4QNAA1EG4eGXIdzAXGU3JbTlXyr2DlRSBObMe/lf3pxiTPQctjoSsLQWw7BtOPpAVbp+OS+lTD8Dut+sva1TYoBnW9KAkU5qkLsKn8uBtb7ozX1rVteHDh1CPGYnKC3nfg5rdOuLlq3xGafE8osEHD/cXEKddtoUwMY+6zroJ7XwsaYvpsa7ArRhARViHKZFwtw9hMmBXR28E93iZDqthaQvfMxjrXBmsFbGptq91EaNp+G0XVH4mP0sJmlQpbI"
+// 	// secretBytes, err := base64.StdEncoding.DecodeString(secret)
+// 	// assert.NoError(err)
 
-	// a2 := []byte("000b6c73dbc157be97f6ee0169b23e608486529cc30becbe7dd277b6822f407a6d53")
-	// n2 := []byte("12345678")
-
-	// log.Infof("aikName[%x]: %s\n\n", len(aikName), hex.EncodeToString(aikName))
-	// log.Infof("a2     [%x]: %s\n\n", len(a2), hex.EncodeToString(a2))
-	// log.Infof("nonce  [%x]: %s\n\n", len(nonce), hex.EncodeToString(nonce))
-	// log.Infof("n2     [%x]: %s\n\n", len(n2), hex.EncodeToString(n2))
+// 	//credential := "ADQAID2qrkbHKt9ZEBb4RdhMh6esz52AHxuqd6LDDtI3pxwxMyYyEGNq0usYQAnW2H4hmggl"
 
 
-	decrypted, err := tpmProvider.ActivateCredential(TpmSecretKey, AikSecretKey, credentialBytes, secretBytes)
-	//decrypted, err := tpmProvider.ActivateIdentity(TpmSecretKey, AikSecretKey, a2, n2)
-	assert.NoError(err)
+// 	credentialBytes, err := ioutil.ReadFile("/tmp/aikName")
+// 	secretBytes, err := ioutil.ReadFile("/tmp/secret.data") //[]byte("12345678")
 
-	//log.Infof("Decrypted: %d", len(decrypted))
-	log.Infof("Decrypted[%x]: %s\n\n", len(decrypted), hex.EncodeToString(decrypted))
-}
+// 	// a2 := []byte("000b6c73dbc157be97f6ee0169b23e608486529cc30becbe7dd277b6822f407a6d53")
+// 	// n2 := []byte("12345678")
+
+// 	// log.Infof("aikName[%x]: %s\n\n", len(aikName), hex.EncodeToString(aikName))
+// 	// log.Infof("a2     [%x]: %s\n\n", len(a2), hex.EncodeToString(a2))
+// 	// log.Infof("nonce  [%x]: %s\n\n", len(nonce), hex.EncodeToString(nonce))
+// 	// log.Infof("n2     [%x]: %s\n\n", len(n2), hex.EncodeToString(n2))
+
+
+// 	decrypted, err := tpmProvider.ActivateCredential(TpmSecretKey, AikSecretKey, credentialBytes, secretBytes)
+// 	//decrypted, err := tpmProvider.ActivateIdentity(TpmSecretKey, AikSecretKey, a2, n2)
+// 	assert.NoError(err)
+
+// 	//log.Infof("Decrypted: %d", len(decrypted))
+// 	log.Infof("Decrypted[%x]: %s\n\n", len(decrypted), hex.EncodeToString(decrypted))
+// }
 
 
 // To run/debug this test (more of a c debugging tool)...
@@ -259,28 +257,27 @@ func TestCreateCertifiedKey(t *testing.T) {
 	assert.Equal(signingKey.Version, V20)
 }
 
-func TestBindingKey(t *testing.T) {
+// func TestBindingKey(t *testing.T) {
 
-	assert := assert.New(t)
+// 	assert := assert.New(t)
 
-	aikSecretKeyBytes, _ := hex.DecodeString(AikSecretKey)
-	certifiedKeySecretBytes, _ := hex.DecodeString(CertifiedKeySecret)
+// 	aikSecretKeyBytes, _ := hex.DecodeString(AikSecretKey)
+// 	certifiedKeySecretBytes, _ := hex.DecodeString(CertifiedKeySecret)
 
-	tpm, err := NewTpmProvider()
-	if assert.NoError(err) == false { return }
-	defer tpm.Close()
+// 	tpm, err := NewTpmProvider()
+// 	if assert.NoError(err) == false { return }
+// 	defer tpm.Close()
 
-	bindingKey, err := tpm.CreateBindingKey(certifiedKeySecretBytes, aikSecretKeyBytes)
-	if assert.NoError(err) == false { return }
+// 	bindingKey, err := tpm.CreateBindingKey(certifiedKeySecretBytes, aikSecretKeyBytes)
+// 	if assert.NoError(err) == false { return }
 
-	var original []byte
-	var encrypted []byte
+// 	encrypted := make([]byte, 32, 32)
 
-	decrypted, err := tpm.Unbind(bindingKey, certifiedKeySecretBytes, encrypted)
-	if assert.NoError(err) == false { return }
+// 	_, err = tpm.Unbind(bindingKey, certifiedKeySecretBytes, encrypted)
+// 	if assert.NoError(err) == false { return }
 
-	assert.Equal(original, decrypted)
-}
+// //	assert.Equal(original, decrypted)
+// }
 
 func TestSigningKey(t *testing.T) {
 
